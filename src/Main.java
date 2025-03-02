40a2480cf96b011874867e06b0377add6c89a843
Davidson Rafael <143698360+YoonaJeon@users.noreply.github.com>
2025-03-02 18:52:31 +0700

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        byte x, y, z;
+        x = y = z = 18;
+        System.out.printf("x: %d y: %d z: %d", x, y, z);
+    }
+}
